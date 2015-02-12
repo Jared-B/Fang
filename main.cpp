858e02a6f1091dcf2414099c1d9a39531a453fdd
Jared B <jared.9@comcast.net>
2015-02-12 01:20:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,15 +12,15 @@ const float kCoxaLength = 45.500;
 const float kFemurLength = 76.976;
 const float kTibiaLength = 102.460;
 const float kNeutralXY = 78.994;
-const float kNeutralZ = -63.548;
+const float kNeutralZ = -63.548; //63
 const float kPI = 3.14159265359;
-const short int kStepSize = 40;
+const short int kStepSize = 30;
 const short int kStepHeight = 20;
-const short int kStepTime = 2000;
-const short int kStepResetTime = 6000;
-const short int kStepTimeOffset [4] = {0,4000,0,4000};
+const short int kStepTime = 500; //600
+const short int kStepResetTime = 600; //700
+const short int kStepTimeOffset [4] = {0,550,0,550};
 const float kWalkAngle = 0;
-
+const short int kOffset = 200; //200ms .2s
 //
 // [FL]    X    [FR]
 //	 #     ^     #
@@ -36,7 +36,7 @@ class Leg;
 BioloidController bioloid = BioloidController(1000000);
 boolean doIK(Leg * a);
 void walk(Leg a[]);
-void writeServos(Leg * a);
+void writeServos(Leg a[]);
 class Leg {
 public:
 	uint8 coxa_id_, femur_id_, tibia_id_, flip_, x_flip_, y_flip_;
@@ -66,7 +66,7 @@ public:
 		doIK(this);
 		/*if(!(checkBounds(this)))
 		return false;*/
-		writeServos(this);
+		//writeServos(this);
 		return true;
 	}
 };
@@ -78,13 +78,34 @@ Leg legs [4] = {Leg (kFrontRight, 1, 1, 1),
 
 
 void setup() {
+        //delay(1000);
+        //Serial.begin(38400);
+        for(int i = 0; i < 3; i++) {
+                 ax12SetRegister(254,25,1);
+                 delay(1000);
+                 ax12SetRegister(254,25,0);
+                 delay(1000);
+                 
+        }
 	for(int i = 0; i < 4; i++) {
 		legs[i].setGoal(0,0,0);
+                writeServos(legs);
 	}
+        delay(50);
+        ax12SetRegister(254,28,64);
+        delay(50);
+        ax12SetRegister(254,29,64);
+        delay(1000);
+        //Serial.begin(38400);
 }
 
+//int lastMillis = 0;
 void loop() {
+        //lastMillis=millis();
 	walk(legs);
+        writeServos(legs);
+        delay(30);
+        //Serial.println(millis()-lastMillis);        
 }
 
 boolean doIK(Leg * a) {
@@ -100,24 +121,76 @@ boolean doIK(Leg * a) {
   return true;
 }
 
-void writeServos(Leg * a) {
-  SetPosition(a->coxa_id_,(int)a->coxa);
-  SetPosition(a->femur_id_,(int)a->femur);
-  SetPosition(a->tibia_id_,(int)a->tibia);
+void writeServos(Leg a[]) {
+	int temp;
+        //unsigned int tempDxl;
+	int length = 4 + (12 * 3);   // 3 = id + pos(2byte) 12 = number of servos
+	int checksum = 254 + length + AX_SYNC_WRITE + 2 + AX_GOAL_POSITION_L;
+	setTXall();
+	ax12write(0xFF);
+	ax12write(0xFF);
+	ax12write(0xFE);
+	ax12write(length);
+	ax12write(AX_SYNC_WRITE);
+	ax12write(AX_GOAL_POSITION_L);
+	ax12write(2);
+	for (int i = 0; i<4; i++)
+	{
+		temp = a[i].coxa;
+		checksum += (temp & 0xff) + ((temp & 0xff00) >> 8) + a[i].coxa_id_;
+		ax12write(a[i].coxa_id_);
+		ax12write(temp & 0xff);
+		ax12write((temp & 0xff00) >> 8);
+
+		temp = a[i].femur;
+		checksum += (temp & 0xff) + ((temp & 0xff00) >> 8) + a[i].femur_id_;
+		ax12write(a[i].femur_id_);
+		ax12write(temp & 0xff);
+		ax12write((temp & 0xff00) >> 8);
+
+		temp = a[i].tibia;
+		checksum += (temp & 0xff) + ((temp & 0xff00) >> 8) + a[i].tibia_id_;
+		ax12write(a[i].tibia_id_);
+		ax12write(temp & 0xff);
+		ax12write((temp & 0xff00) >> 8);
+	}
+	ax12write(0xff - (checksum % 256));
+	setRX(0);
 }
+
+
+
 
 void walk(Leg a[]) {
 	for(int j = 0; j < 4; j++) {
-		int time = millis()%(kStepTime+kStepResetTime+kStepTimeOffset[j]);
+		int time = (millis()+kStepTimeOffset[j])%(kStepTime+kStepResetTime);
 		if(time <= kStepTime) {
-			a[j].setGoal(a[j].x_flip_*2*kStepSize*cos(kWalkAngle)/2*((sin(0.5*(2*kPI)/kStepTime*time-kPI/2))),
-						 a[j].y_flip_*2*kStepSize*sin(kWalkAngle)/2*((sin(0.5*(2*kPI)/kStepTime*time-kPI/2))),
-						 kStepHeight/2*((sin((2*kPI)/kStepTime*time-kPI/2))+1));
+			a[j].setGoal(a[j].x_flip_*2*kStepSize*cos(kWalkAngle)/2.0*((sin(0.5*(2*kPI)/kStepTime*time-kPI/2))),
+				     a[j].y_flip_*2*kStepSize*sin(kWalkAngle)/2.0*((sin(0.5*(2*kPI)/kStepTime*time-kPI/2))),
+				     zStep(time));
 		}
-		else
-			a[j].setGoal(a[j].x_flip_*kStepSize*cos(kWalkAngle)*(1-2.0/kStepResetTime*(time-kStepTime)),
-						 a[j].y_flip_*kStepSize*sin(kWalkAngle)*(1-2.0/kStepResetTime*(time-kStepTime)),
-						 0);
+		else if((time <= kStepTime+kOffset)&&(time>kStepTime))
+                    a[j].setGoal(a[j].x_flip_*kStepSize*cos(kWalkAngle)*(1-2.0/(float)kStepResetTime*(time-kStepTime)),
+		        a[j].y_flip_*kStepSize*sin(kWalkAngle)*(1-2.0/(float)kStepResetTime*(time-kStepTime)),
+  		         zStep(time));
+                else if (time>=(kStepTime+kStepResetTime-kOffset))
+               		a[j].setGoal(a[j].x_flip_*kStepSize*cos(kWalkAngle)*(1-2.0/(float)kStepResetTime*(time-kStepTime)),
+			  	     a[j].y_flip_*kStepSize*sin(kWalkAngle)*(1-2.0/(float)kStepResetTime*(time-kStepTime)),
+				     zStep(time-kStepTime-kStepResetTime));   
+                 else// if(((time+kOffset)<(kStepTime+kStepResetTime))&&(time>(kStepTime+kOffset)))
+                                   a[j].setGoal(a[j].x_flip_*kStepSize*cos(kWalkAngle)*(1-2.0/kStepResetTime*(time-kStepTime)),
+				                  a[j].y_flip_*kStepSize*sin(kWalkAngle)*(1-2.0/kStepResetTime*(time-kStepTime)),
+				                  -kStepHeight/10.0*cos(kPI/(kStepResetTime-kOffset)*(time-kStepTime-kOffset/2)-kPI/2));
 	}
+        
 
+}
+
+float zStep (int time){
+  return kStepHeight/2.0*((sin((2*kPI)/(kStepTime+2*kOffset)*(time+kOffset)-kPI/2))+1); 
+  
+  // return kStepHeight/2.0*((sin((2*kPI)/(kStepTime+2*kOffset)*(time+kOffset)-kPI/2))+1);
+  //if(z<0);
+  //  z=0;
+  //return z;
 }
